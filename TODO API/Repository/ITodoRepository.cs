c1b7315f568e5123c2f35060f78e97211ad663ca
Raquel <raquelsorila@gmail.com>
2021-04-26 03:29:46 +0800

    Created Todo Repository

---
+++ TODO API/Repository/ITodoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TODO_API.Models;
+
+namespace TODO_API.Repository
+{
+    public interface ITodoRepository
+    {
+        Task<IEnumerable<Todo>> Get();
+        Task<Todo> Get(double id);
+        Task<Todo> Create(Todo todo);
+        Task Update(Todo todo);
+        Task Delete(double ID);
+    }
+}
---
+++ TODO API/Repository/TodoRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TODO_API.Models;
+
+namespace TODO_API.Repository
+{
+    public class TodoRepository : ITodoRepository
+    {
+        private readonly TodoDbContext _context;
+        public TodoRepository(TodoDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Todo> Create(Todo todo)
+        {
+            _context.Todos.Add(todo);
+            await _context.SaveChangesAsync();
+
+            return todo;
+        }
+        public Task Delete(double ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Todo>> Get()
+        {
+            return await _context.Todos.ToListAsync();
+        }
+
+        public async Task<Todo> Get(double id)
+        {
+            return await _context.Todos.FindAsync(id);
+        }
+
+        public Task Update(Todo todo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
