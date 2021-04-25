11e7a3af2982f79e7941f3abc5648bbbe5ff2259
larzthimotypal-ing <larzthimoty2421@gmail.com>
2021-04-26 04:34:39 +0800

    Added Migration

---
+++ TODO API/Migrations/20210425201802_initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TODO_API.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todos",
+                columns: table => new
+                {
+                    ID = table.Column<double>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Completed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todos", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todos");
+        }
+    }
+}
--- TODO API/Startup.cs
+++ TODO API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TODO_API.Models;
 using TODO_API.Repository;
 
 namespace TODO_API
@@ -26,7 +28,10 @@ namespace TODO_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped<ITodoRepository, TodoRepository>();
+            services.AddCors();
             services.AddControllers();
         }
 
@@ -43,6 +48,10 @@ namespace TODO_API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(
+               options => options.SetIsOriginAllowed(x => _ = true).AllowAnyMethod().AllowAnyHeader().AllowCredentials()
+           );
 
             app.UseEndpoints(endpoints =>
             {
