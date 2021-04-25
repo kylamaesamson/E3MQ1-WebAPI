612d256cae09a561299aed31588187a509113512
kylamae <kylasamson0725@gmail.com>
2021-04-26 03:03:08 +0800

    Init

---
+++ TODO API/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODO_API.Models
+{
+    public class Todo
+    {
+        public double ID { get; set; }
+        public string Name { get; set; }
+        public bool Completed { get; set; }
+    }
+}
---
+++ TODO API/Models/TodoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODO_API.Models
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
